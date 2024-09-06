f8001b4d5d7ee8b74255e9276fe1625fd23f151c
JessicaRMoreira <tjjessicatj@gmail.com>
2024-09-06 13:10:33 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo: "Definição da Espécie",
+        descricao: "A onça-pintada é o maior felino das Américas, conhecida por sua beleza e força. Sua pelagem característica, amarelo-dourada com rosetas negras, serve como camuflagem perfeita em seu habitat. Com seu corpo robusto e musculoso, aliado a uma mordida extremamente poderosa, a onça-pintada se destaca como um predador ápice em diversos ecossistemas.",
+        link: "https://mundoeducacao.uol.com.br/biologia/onca.htm",
+        tags: "características  caracteristicas expectativa de vida peso especie taxonomia" 
+    },
+    {
+        titulo: "Curiosidades",
+        descricao: "O Dia Internacional da Onça-Pintada é comemorado em 29 de novembro. A data foi estabelecida pelo Programa das Nações Unidas para o Meio Ambiente (PNUMA) como uma forma de homenagear o felino e chamar atenção para a sua proteção.",
+        link: "https://portalamazonia.com/amazonia/conheca-dez-curiosidades-do-maior-felino-das-americas/",
+        tags: "jaguar pantera melanina onca-preta onça-preta onca preta onça preta"
+    },
+    {
+        titulo: "Projetos de Conservação",
+        descricao: "Diversas organizações trabalham para proteger a onça-pintada e seu habitat. Esses projetos incluem ações como monitoramento da população, combate ao tráfico de animais, criação de áreas protegidas e educação ambiental. Através dessas iniciativas, busca-se garantir a sobrevivência desse magnífico felino e a preservação dos ecossistemas onde ele vive.",
+        link: "https://oncafari.org/",
+        tags: "extinção extincao queimadas desmatamento caça caca oncafari onçafari conservacao"
+    }
+];
+
+
